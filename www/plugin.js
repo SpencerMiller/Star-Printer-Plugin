aaf269e9b263a4c43f3e7476c4732af2e9b534a8
Jared Adams <jared@watchmantech.com>
2017-02-22 10:41:57 -0700

    Basic Android functionality

---
+++ www/plugin.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var exec = require('cordova/exec');
+
+module.exports = {
+    findDevices: function () {
+        return new Promise(function (resolve, reject) {
+            exec(resolve, reject, 'OmniPrinter', 'findDevices');
+        });
+
+    },
+
+    status: function(device) {
+        return new Promise(function (resolve, reject) {
+            exec(resolve, reject, 'OmniPrinter', 'status', [device]);
+        });
+    },
+
+    print: function(device, content) {
+        return new Promise(function (resolve, reject) {
+            exec(resolve, reject, 'OmniPrinter', 'print', [device, content]);
+        });
+    },
+};
