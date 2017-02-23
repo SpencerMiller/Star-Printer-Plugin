651d244d24c0926838cb633abbec7d59d57716e9
Jared Adams <jared@watchmantech.com>
2017-02-23 11:09:41 -0700

    Cleanup and simplify Android side

--- src/android/net/omnidispatch/ionic/OmniPrinterPlugin.java
+++ src/android/net/omnidispatch/ionic/OmniPrinterPlugin.java
@@ -1,15 +1,7 @@
 package net.omnidispatch.ionic;
 
-import java.io.BufferedOutputStream;
-import java.io.OutputStream;
-import java.util.List;
 import java.util.Set;
-import java.util.UUID;
 
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
-import android.os.ParcelUuid;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
@@ -18,15 +10,19 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.starmicronics.stario.PortInfo;
-import com.starmicronics.stario.StarIOPort;
-
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Context;
+import android.os.ParcelUuid;
 import android.util.Log;
 
 public class OmniPrinterPlugin extends CordovaPlugin {
     static final String TAG = "OmniPrinter";
     static final int TIMEOUT = 10000;
+
+    static String formatResultError(Throwable t) {
+        return t.getMessage() + "\n" + Log.getStackTraceString(t);
+    }
 
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
@@ -45,8 +41,7 @@ public class OmniPrinterPlugin extends CordovaPlugin {
         } else if (action.equals("print")) {
             String device = args.getString(0);
             String content = args.getString(1);
-//            cordova.getThreadPool().submit(new PrintExec(device, content, callbackContext, getContext()));
-            print(content, callbackContext);
+            cordova.getThreadPool().submit(new PrintExec(device, content, callbackContext));
         }
         return true;
     }
@@ -100,37 +95,5 @@ public class OmniPrinterPlugin extends CordovaPlugin {
         }
 
         callbackContext.success(result);
-    }
-
-    private void print(String toPrint, CallbackContext callbackContext) throws JSONException {
-        try {
-            BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
-
-            BluetoothDevice device = adapter.getRemoteDevice("00:15:0E:E5:71:C6");
-            BluetoothSocket socket = device.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"));
-            socket.connect();
-            OutputStream out = new BufferedOutputStream(socket.getOutputStream());
-
-            out.write(new byte[]{0x1b, 0x40}); // ESC @ - Initialize printer
-
-            out.write(toPrint.getBytes());
-
-            // x lines for stamp + 5 to feed past cutter
-            out.write(new byte[]{0x1b, 0x64, 5}); // ESC d n - Prints the data in the print buffer and feeds n lines
-            out.flush();
-            // none of out.flush(), out.close() nor socket.close() appear to wait for the buffer to flush so we wait here
-            try {
-                Thread.sleep(10000);
-            } catch (InterruptedException e) {
-                Log.e("LOG", Log.getStackTraceString(e));
-            }
-            out.close();
-
-            callbackContext.success("Done");
-//        } catch (JSONException e) {
-//            throw e;
-        } catch (Exception e) {
-            callbackContext.error(PrinterExec.formatResultError(e));
-        }
     }
 }
