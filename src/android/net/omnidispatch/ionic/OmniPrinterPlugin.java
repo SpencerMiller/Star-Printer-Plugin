1bec666fb5dd2fd53c658439ad7bbdc0dccc963f
Jared Adams <jaxad0127@gmail.com>
2017-03-02 11:03:38 -0700

    Remove Star Printer driver from Android

--- src/android/net/omnidispatch/ionic/OmniPrinterPlugin.java
+++ src/android/net/omnidispatch/ionic/OmniPrinterPlugin.java
@@ -1,7 +1,9 @@
 package net.omnidispatch.ionic;
 
-import java.util.Set;
-
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.os.ParcelUuid;
+import android.util.Log;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
@@ -10,15 +12,10 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.content.Context;
-import android.os.ParcelUuid;
-import android.util.Log;
+import java.util.Set;
 
 public class OmniPrinterPlugin extends CordovaPlugin {
     static final String TAG = "OmniPrinter";
-    static final int TIMEOUT = 10000;
 
     static String formatResultError(Throwable t) {
         return t.getMessage() + "\n" + Log.getStackTraceString(t);
@@ -35,9 +32,6 @@ public class OmniPrinterPlugin extends CordovaPlugin {
         Log.w(TAG, "Exec " + action);
         if (action.equals("findDevices")) {
             findDevices(callbackContext);
-        } else if (action.equals("status")) {
-            String address = args.getString(0);
-            cordova.getThreadPool().submit(new PrinterStatusExec(address, callbackContext, getContext()));
         } else if (action.equals("print")) {
             String address = args.getString(0);
             JSONArray content = args.getJSONArray(1);
@@ -46,29 +40,7 @@ public class OmniPrinterPlugin extends CordovaPlugin {
         return true;
     }
 
-    private Context getContext() {
-        return cordova.getActivity().getApplicationContext();
-    }
-
     private void findDevices(CallbackContext callbackContext) throws JSONException {
-//        try {
-//            List<PortInfo> ports = StarIOPort.searchPrinter("BT:", getContext());
-//            JSONArray result = new JSONArray();
-//
-//            for (PortInfo port : ports) {
-//                JSONObject obj = new JSONObject();
-//                result.put(obj);
-//
-//                obj.put("mac", port.getMacAddress());
-//                obj.put("usbSerial", port.getUSBSerialNumber());
-//                obj.put("model", port.getModelName());
-//                obj.put("port", port.getPortName());
-//            }
-//
-//            callbackContext.success(result);
-//        } catch (Exception e) {
-//            callbackContext.error(PrinterExec.formatResultError(e));
-//        }
         BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
         Set<BluetoothDevice> pairedDevices = adapter.getBondedDevices();
         JSONArray result = new JSONArray();
